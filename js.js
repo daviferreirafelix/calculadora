edb1d68fc837e5793b0b2589a336571012e30df0
daviferreirafelix <adavi9920@gmail.com>
2023-02-23 09:13:02 -0300

    Solucionado a função do botão "limpar"

--- js.js
+++ js.js
@@ -30,50 +30,67 @@ let numbers = [
 ];
 
 let numbersClicked = [];
-
-numbers.forEach((i) => {
-  i.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", i.value);
-    numbersClicked.push(i.value);
-    console.log(numbersClicked);
-  });
-});
-
+let novoArray = [];
 let calculo = 0;
 
-let calculos = {
-  soma: simbols.botaoSomar.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "+");
-  }),
-  subtracao: simbols.botaoSubtrair.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "-");
-  }),
-  divisao: simbols.botaoDividir.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "/");
-  }),
-  multipli: simbols.botaoMultiplicar.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "*");
-  }),
-  elevarQuadrado: simbols.botaoElevarQuadrado.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "^");
-  }),
-  porcentagem: simbols.botaoPorcentagem.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "%");
-  }),
-  raiz: simbols.botaoRaiz.addEventListener("click", () => {
-    resultado.insertAdjacentText("beforeend", "√");
-  }),
-  igual: simbols.sinalIgual.addEventListener("click", () => {
-    if (resultado.innerHTML === simbols.botaoSomar.textContent) {
-      console.log("tem o sinal de somar");
-      calculo = numbersClicked.reduce((acumulador, valorAtual) => {
-        return acumulador + valorAtual;
-      })
-      resultado.insertAdjacentText("beforeend", calculo);
-    }
-    resultado.insertAdjacentText("beforeend", ` = ${calculo}`);
-  }),
-  limpar: simbols.botaoLimpar.addEventListener("click", () => {
-    resultado.innerHTML = "0";
-  }),
-};
+numbers.forEach((numbersClickedValue) => {
+  numbersClickedValue.addEventListener("click", () => {
+    numbersClicked.push(numbersClickedValue.value);
+    console.log(numbersClicked);
+    resultado.textContent == 0
+      ? (resultado.textContent = numbersClicked[0])
+      : resultado.insertAdjacentText("beforeend", numbersClickedValue.value);  
+    });
+});
+
+simbols.botaoSomar.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", " + ");
+  console.log(Number(numbersClicked.join("")));
+  for (let numbersNovoArray in novoArray.length){
+    numbersNovoArray.add
+  } 
+});
+simbols.botaoSubtrair.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", "-");
+});
+simbols.botaoDividir.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", "/");
+});
+simbols.botaoMultiplicar.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", "*");
+});
+simbols.botaoElevarQuadrado.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", "^");
+});
+simbols.botaoPorcentagem.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", "%");
+});
+simbols.botaoRaiz.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", "√");
+});
+simbols.botaoVirgula.addEventListener("click", () => {
+  resultado.insertAdjacentText("beforeend", simbols.botaoVirgula.textContent);
+  numbersClicked.push(simbols.botaoVirgula.textContent);
+});
+simbols.sinalIgual.addEventListener("click", () => {
+  if (resultado.textContent !== simbols.botaoSomar.textContent) {
+    console.log("tem o sinal de somar");
+    calculo = numbersClicked + novoArray;
+  } else if (resultado.innerHTML === simbols.botaoSubtrair.textContent) {
+    console.log("Tem o sinal de subtrair");
+    calculo = numbersClicked.reduce((acumulador, valorAtual) => acumulador - valorAtual);
+  } else if (resultado.innerHTML === simbols.botaoDividir.textContent) {
+    console.log("Tem o sinal de dividir");
+    calculo = numbersClicked.reduce((acumulador, valorAtual) => acumulador / valorAtual);
+  } else if (resultado.innerHTML === simbols.botaoMultiplicar.textContent) {
+    console.log("Tem o botão de multiplicar");
+    calculo = numbersClicked((acumulador, valorAtual) => acumulador * valorAtual);
+  }
+  resultado.insertAdjacentText("beforeend", ` = ${calculo}`);
+});
+
+//RESOLVIDO
+simbols.botaoLimpar.addEventListener("click", () => {
+  resultado.textContent = "0";
+  while (numbersClicked.length) numbersClicked.pop();
+});
